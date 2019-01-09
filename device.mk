ea07c3f971f075cdea84d5efbe78eb5ae29fb22f
Sᴀᴍᴀʀ Vɪꜱᴘᴜᴛᴇ <samarvispute121@gmail.com>
2019-01-09 15:34:59 +0530

    Update device.mk

--- device.mk
+++ device.mk
@@ -61,7 +61,6 @@ PRODUCT_COPY_FILES += \
 # Media
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/media/media_codecs.xml:system/etc/media_codecs.xml \
-    $(LOCAL_PATH)/configs/media/media_codecs_mediatek_video.xml:system/etc/media_codecs_mediatek_video.xml \
     $(LOCAL_PATH)/configs/media/media_codecs_performance.xml:system/etc/media_codecs_performance.xml \
     $(LOCAL_PATH)/configs/media/media_profiles.xml:system/etc/media_profiles.xml \
     $(LOCAL_PATH)/configs/media/mtk_omx_core.cfg:system/vendor/etc/mtk_omx_core.cfg
